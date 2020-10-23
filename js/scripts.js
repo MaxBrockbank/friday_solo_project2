04f8087651aeac506b25356a9255ff39784f5d9c
Max Brockbank <maxbrockbank1999@gmail.com>
2020-10-23 11:07:38 -0700

    finish adding possible results based on score

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,9 @@ $(document).ready(function(){
   $("#start").click(function(){
     $("#starting-page").hide();
   })
+  $("#retake").click(function(){
+    location.reload();
+  })
 
   $("#language-quiz").submit(function(event){
 
@@ -14,7 +17,7 @@ $(document).ready(function(){
     const coffee = $("input:radio[name=coffee]:checked").val();
     const stressed = $("input:radio[name=stressed]:checked").val();
     const problem= $("input:radio[name=problem]:checked").val();
-    const results = $("#results-page h1");
+    const results = $("#results");
     let score = 0;
 
     //Scoring Logic
@@ -49,9 +52,14 @@ $(document).ready(function(){
     }
 
     if(score === 0){
-      results.text("If you don't like probelm solving then maybe programming isnt for you...")
-    }
-
+      results.html("If you don't like probelm solving then maybe programming <strong>isnt</strong> for you...");
+    } else if (score > 0 && score < 10){
+      results.html("It sounds like <em>front-end web development</em> would be up your ally! Check out learning <strong>JavaScript</strong>!");
+    } else if (score === 10){
+      results.html("It sounds like <em>full-stack web development</em> would be up your ally! Check out learning <strong>JavaScript</strong> for front-end and <strong>C#/.NET</strong> for back-end!");
+    } else {
+      results.html("It sounds like <em>software development</em> is up your ally! Check out learning <strong>Swift</strong> for iOS mobile apps or <strong>Python</strong> for general software!");
+    };
     $("#results-page").show();
   })
 

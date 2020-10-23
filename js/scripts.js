1986dad3e375034360b743945fecb77d59163ffe
Max Brockbank <maxbrockbank1999@gmail.com>
2020-10-23 10:40:51 -0700

    add basic scoring logic and results overlay

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,55 @@ $(document).ready(function(){
     $("#starting-page").hide();
   })
 
+  $("#language-quiz").submit(function(event){
+
+    event.preventDefault();
+
+    const job = $("#job").val();
+    const breakfast = $("#breakfast").val();
+    const coffee = $("input:radio[name=coffee]:checked").val();
+    const stressed = $("input:radio[name=stressed]:checked").val();
+    const problem= $("input:radio[name=problem]:checked").val();
+    const results = $("#results-page h1");
+    let score = 0;
+
+    //Scoring Logic
+    if(job === "web"){
+      score += 2;
+    } else {
+      score += 10;
+    }
+
+    if(breakfast === "waffle"){
+      score += 1;
+    } else{
+      score += 2;
+    }
+
+    if(coffee === "1cup" || coffee === "5cup"){
+      score += 1;
+    } else {
+      score += 2;
+    }
+
+    if(stressed === "hair" || stressed === "nail"){
+      score += 1;
+    } else {
+      score += 2;
+    }
+
+    if(problem === "yes"){
+      score += 2;
+    } else {
+      score = 0;
+    }
+
+    if(score === 0){
+      results.text("If you don't like probelm solving then maybe programming isnt for you...")
+    }
+
+    $("#results-page").show();
+  })
 
 
 })
